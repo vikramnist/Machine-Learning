b55f953aaa3f4814b7c7692c15bbf616d40695ee
vikramnist <50799962+vikramnist@users.noreply.github.com>
2020-06-12 23:07:44 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,33 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('hiring.csv')
+
+dataset['experience'].fillna(0, inplace=True)
+
+dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
+
+X = dataset.iloc[:, :3]
+
+
+
+y = dataset.iloc[:, -1]
+
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+#Fitting model with trainig data
+regressor.fit(X, y)
+
+# Saving model to disk
+pickle.dump(regressor, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[2, 9, 6]]))
